2d0bcc4e2168265a2e24996adfc738e769cdc216
SumeyraYazgan <sumeyra.yazgan@agu.edu.tr>
2018-05-10 19:47:21 +0300

    Add files via upload

---
+++ PlotShape.m
@@ -0,0 +1,15 @@
+function [  ] = PlotShape(xval,yval,zval,C,alpha )
+
+
+ figure(1)
+    fill3(xval,yval,zval,C,'FaceAlpha',alpha);
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+    xlim([-2 2]);
+    ylim([-2 2]);
+    zlim([-2 2]);
+    box on;
+    drawnow
+end
+
